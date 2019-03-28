06504154a1d852e60e0779811dddfd6d0e8397c5
princeIta <princeIta@outlook.com>
2019-03-28 12:46:49 +0100

    added logic to add category and sub category to store

--- routes/store.js
+++ routes/store.js
@@ -5,15 +5,11 @@ const path = require('path');
 let express = require('express');
 let router = express.Router();
 var mongoose = require('mongoose')
-const { check, validationResult } = require('express-validator/check');
-const { sanitizeParam } = require('express-validator/filter');
 const formidable = require('formidable');
-const serveStoreImgs = express.Router();
 // const Store = require('../models/store').Store;
 
 const {
     Store: Store, 
-    Address: StoreAddress, 
     item: Product, 
     itemCategory: ProductCategory, 
     itemSubCategory: ProductSubCategory} = require('../models/store');
@@ -32,15 +28,19 @@ router.use('/:storeid',
         // the storeid retrived in params he is about to access
         store = stores.find((store)=>store.storeid == storeid)
         if(store != undefined){
-            Store.findById(store.storeid, function(err, storeRes){
+            Store.findById(store.storeid, function(err, store){
                 if(err) return next(err);
                 // complete refernce object to the store object 
-                res.locals.store = storeRes;
-                // console.log('HUGE', res.locals)
+                res.locals.store = store;
             });
-            // user's refernce object to the store object 
-            // contains => storeid, storename, role
-            // res.locals.currentStore = store;
+
+            ProductCategory.find({store: storeid}, 'category_name _id', function(err, cat){
+                if(err) return next(err);
+                // console.log(cat)
+                res.locals.categories = cat;
+                
+            });
+
             return next();
         }
     
@@ -51,16 +51,9 @@ router.use('/:storeid',
     }
 );
 
-// this route will handle serving every image required on the store
-// serveStoreImgs.get('/:imageName', function(req, res,next){
-    
-
-// });
-
-router.get('/:storeid/images/:imageName', function(req, res, next){
+router.get('/:storeid/images/:imageName', function(req, res){
     var imageName = req.params.imageName;
     var store = req.params.storeid
-    console.log('HUGE', store)
     var dataDirectoryRoot = req.dataDirectoryRoot;
     res.sendFile(path.join(dataDirectoryRoot, store, imageName))
 });
@@ -68,38 +61,40 @@ router.get('/:storeid/images/:imageName', function(req, res, next){
 router.get('/:storeid', function(req, res, next){
     const storeid = req.params.storeid;
     res.locals.products = undefined;
+    // console.log(res.locals)
     
-    // get store information
-    
-
     Product.where('store').equals(storeid).exec(function(err, products){
         if(err){
             return next(err)
         }
 
-        // something in products?
-        if(products.length){
-            // console.log('products: ', {page: 'store', products}, typeof products)
-            res.render('vendor-store', {page: 'store', products, storeid})
-        }
-        else{
-            res.render('vendor-store', {page: 'store', storeid})
-        }
+        ProductCategory.find({store: storeid}, 'category_name _id', function(err, cat){
+            if(err) return next(err);
+            // console.log(cat)
+            categories = cat;
+            // console.log(cat)
+            // something in products?
+            if(products.length){
+                // console.log('products: ', {page: 'store', products}, typeof products)
+                res.render('vendor-store', {page: 'store', categories: categories, products, storeid})
+            }
+            else{
+                res.render('vendor-store', {page: 'store', categories: categories, storeid})
+            }
+            
+        });
         
     })
 })
 
-// router.get('/:storeid/image/:imagepath', function(req, res, next){
-
-// });
-
+// create or change store logo
 router.post('/:storeid/logo', function(req, res, next){
     var dataDirectoryRoot = req.dataDirectoryRoot;  // root directory for storing images
 
     var form  = new formidable.IncomingForm();
 
     var storeid =  req.params.storeid;
-
+    
     // console.log(storeid)
     var storeDataDir = ''+storeid
     // console.log(storeDataDir)
@@ -191,5 +186,133 @@ router.post('/:storeid/banner', function(req, res){
         })
     });
 });
+
+router.post('/:storeid/cat',
+    function(req, res){
+        // console.log(res.locals)
+        var dataDirectoryRoot = req.dataDirectoryRoot;  // root directory for storing images
+
+        var form  = new formidable.IncomingForm();
+
+        var storeid =  req.params.storeid;
+        
+        var response = {};
+
+        // console.log(res.locals)
+        var storeDataDir = ''+storeid
+        // console.log(storeDataDir)
+        if(!fs.existsSync(path.join(dataDirectoryRoot, storeDataDir))){
+            fs.mkdirSync(path.join(dataDirectoryRoot, storeDataDir), {recursive: true});
+        }
+
+        form.parse(req, (err, fields, files) => {
+            var catName = fields.catName;
+            if(!catName){
+                response.success = false;
+                response.message = 'please do not leave the category name field blank';
+                return res.json(response);
+            }
+
+            // console.log(catName)
+            if (err) {
+                response.success = false;
+                response.message = 'something went wrong, please try again';
+                return res.json(response);
+            } 
+
+            // we dont want a store to have two or more categories with the same name
+            ProductCategory.find({store: storeid, category_name: catName.toLowerCase()}, 'category_name', 
+                function(err, cat){
+                    if(cat && cat.length){
+                        console.log(cat)
+                        response.success = false;
+                        response.message = "you already a category with that name";
+                        return res.json(response);
+                    }else{
+                        var storeDataDir = ''+storeid
+                        var newImgName = ''+mongoose.Types.ObjectId() // .Schema.Types.ObjectId;
+                        var newImgPath = path.join(dataDirectoryRoot, storeDataDir, newImgName);
+                        var oldImgPath = files.file.path;
+                        
+                        fs.rename(oldImgPath, newImgPath, (err) => {
+                            if (err){
+                                response.success = false;
+                                response.message = 'something went wrong, please try again';
+                                return res.json(response);
+                               
+                            };
+                            // console.log('Rename complete!');
+                            var productCategory = new ProductCategory({category_name: catName, image_path: newImgName, store: storeid});
+                            
+                            productCategory.save(function(err, category){
+                                if(err) {
+                                    response.success = false;
+                                    response.message = 'something went wrong, please try again';
+                                    return res.json(response);
+                                   
+                                }
+                                response.success = true;
+                                response.message = 'success'
+                                response.data = {}
+                                response.data.value = category._id
+                                response.data.innerHtml = category.category_name
+                                return res.json(response);
+                                
+                            });    
+                        })
+                    }
+                }
+            );
+        });
+    }
+);
+
+router.post('/:storeid/subcat',
+    function(req, res){
+        var categoryId = req.body.subcatCategory;
+        var subCategoryName = req.body.subCatName;
+        var storeid = req.params.storeid;
+        var response = {};
+
+        var form  = new formidable.IncomingForm();
+        form.parse(req, (err, fields) => {
+            categoryId = fields.subcatCategory;
+            subCategoryName = fields.subCatName;
+            console.log(categoryId, subCategoryName)
+            if(!subCategoryName){
+                response.success = false;
+                response.message = 'please do not leave the sub category name field blank';
+                return res.json(response);
+            }
+            
+            ProductSubCategory.find({category: categoryId, name: subCategoryName.toLowerCase()}, function(err, subcats){
+                if(err) return res.json({success: false, message: 'something went wrong please try again'});
+                
+                if(subcats && subcats.length){
+                   return res.json({success: false, message: 'this category already has a sub category of name '+ subCategoryName}) 
+                }
+                ProductCategory.findById(categoryId, function(err, cat){
+                    console.log('cat', cat)
+                    if(cat && cat._id){
+                        var productSubCategory = new ProductSubCategory({category: categoryId, store: storeid, name: subCategoryName})
+                        productSubCategory.save(function(err, subcat){
+                            cat.subCategories.push(subcat);
+                            cat.save(function(){
+                                response.success = true;
+                                response.message = 'success';
+                                return res.json(response)
+                            });
+                        });
+                    }
+                    else{
+                        response.success = false;
+                        response.message = 'category does not exist';
+                        return res.json(response)
+                    }
+                });
+            });
+        })
+    }
+);
 
 module.exports = router;
