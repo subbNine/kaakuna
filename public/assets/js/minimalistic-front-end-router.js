638effcdbcaa1675eaa53eaea8f7f7204ea98390
princeIta <princeIta@outlook.com>
2019-03-31 06:44:07 +0100

    improved front-end routing

--- public/assets/js/minimalistic-front-end-router.js
+++ public/assets/js/minimalistic-front-end-router.js
@@ -1,45 +1,72 @@
 const minfroutes = {};
 const minfhandlers = {};
+const minfviews = {};
 var minfcontainer = undefined;
+var minfOldRoute = '';
 // register route handlers
-const minfhandler = function(name, handlerFunc){
-    return minfhandlers[name] = handlerFunc;
-};
+// const minfhandler = function(name, handlerFunc){
+//     return minfhandlers[name] = handlerFunc;
+// };
 
 // define routes
 // when you specify a route with its handler 
-// the handler can either be a function or a string that represents
-// the name of an already registered handler
-const minfroute = function(path, handler){
+// the handler is a function that returns a view
+const minfroute = function(path, handler, ...callbacks){
+	if(callbacks){
+		minfroutes[path] = {callbacks: callbacks};
+	}
+	
     if(typeof(handler) === 'function'){
-        return minfroutes[path] = handler;
-    }else if(typeof(handler) === 'string'){
-        return minfroutes[path] = minfhandlers[handler];
-    }else{
+		minfviews[path] = handler();	
+	}
+    else{
         return;
+    }
+};
+
+const minfgetView = function(route){
+	if(!minfviews[route]){
+		try{
+			return minfviews['*']
+		}
+		catch(err){
+			minfviews['*'] = '<p>not found<p/>'
+			return minfviews['*']
+		}
+    }else{
+		console.log(route)
+        return minfviews[route];
     }
 };
 
 const minfresolveRoute = function(route){
     // console.log(minfroutes);
-    if(!minfroutes[route]){
-        return minfroutes['*']
-    }else{
-        return minfroutes[route];
-    }
+	if(minfOldRoute && minfOldRoute in minfviews){ 
+		if(minfcontainer && minfcontainer.innerHTML){
+			minfviews[minfOldRoute] = minfcontainer.innerHTML;
+			
+		}
+	}
+	minfOldRoute = route;
+	minfcontainer.innerHTML = minfgetView(route);	
+	
+	var callbacks = minfroutes[route].callbacks
+	if(callbacks && callbacks.length){
+		callbacks.forEach(callback=>callback());
+	}
 };
 
 const minfrouter = (evt) => {
-    if(minfcontainer){
-        minfcontainer.innerHTML = ''
-    }
     const url = window.location.hash.slice(1) || "/";
-    const routeResolved = minfresolveRoute(url);
-    routeResolved();
+    minfresolveRoute(url);
 };
 
 const minfredirect = (redirectTo)=>{
     window.location.hash = redirectTo;
+}
+
+function minfsetContainer(containerDiv){
+	minfcontainer = containerDiv;
 }
 
 // For first load or when routes are changed in browser url box.
